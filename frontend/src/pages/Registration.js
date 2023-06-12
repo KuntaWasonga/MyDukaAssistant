230e3350b8b021a75ad8a5660440b2e65f66580b
KuntaWasonga <wkunta@rocketmail.com>
2023-06-12 09:11:15 +0300

    Added return content

--- frontend/src/pages/Registration.js
+++ frontend/src/pages/Registration.js
@@ -3,7 +3,7 @@ import React, { } from "react";
 
 import { Link } from 'react-router-dom';
 
-export default function LoginPage() {
+export default function RegistrationPage() {
 
 	//Add a check: client | employee.
 	//Employee: If so, add text box for their ID. If invalid, mention as so once they submit.
@@ -11,14 +11,40 @@ export default function LoginPage() {
 
 	return (
 		<div>
-			<div className="bg-img">
-				<Link to="/login"><input type="button" onclick="success();" value="submit" /> </Link>
-			</div>
-			<script>
-				function success() {
-					alert("Your account was created successfully")
-				};
-			</script>
+			<form action="registration" style="border:1px solid #ccc">
+				<div className="container">
+					<h1>SIGN UP</h1>
+					<p>Kindly fill this to create an account</p>
+
+					<hr>
+					<label><b>E-mail</b></label>
+					<input type="text" placeholder="Enter e-mail address"
+					name="email" required/>
+
+					<label><b>Password</b></label>
+					<input type="text" placeholder="Enter a password"
+					name="password" required/>
+
+					<label><b>E-mail</b></label>
+					<input type="text" placeholder="Repeat password"
+					name="rpassword" required/>
+
+					<label>
+						<input type="checkbox" checked="checked"
+						name="remember" style="margin-bottom:15px"/>
+					</label>
+
+					<p>By creating an account you agree to our
+						<a href="#terms" style="color:dodgerblue">Terms & Privacy</a>
+					</p>
+
+					<div>
+						<button type="button" class="cancelbtn"></button>
+						<button type="submit" class="signupbtn"></button>
+					</div>
+					</hr>
+				</div>
+			</form>
 		</div>
 	);
 }
