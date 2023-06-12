be82319955be147df1c8ac8032f64053fd957daf
KuntaWasonga <wkunta@rocketmail.com>
2023-06-12 08:37:22 +0300

    login page

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,26 +1,21 @@
 //This contains code for the landing page of the web application
 import React, { } from "react";
  
-import {Link} from 'react-router-dom';
+//import {Link} from 'react-router-dom';
  
-export default function HomePage(){
-  let y = 22;
+export default function LoginPage(){
 
-  if (window.location.pathname === "/login") {
-    y += 1;
-  } else {
-    y -=1;
-  }
- 
+  //This is a form that should pop up on the 
+  //Check if user in employee or client database.
+  //Check for the necessary password with the hash key.
+  //If employee, go to employee home page.
+  //If client, go to client home page.
+
   return (
     <div>
-        <div className="container h-100">
-            <div className="row h-100">
-                <div className="col-12">
-                    <h1>Welcome to Jabali supermarket {y}</h1>
-                    <p><Link to="/login" className="btn btn-success">Scan</Link> | <Link to="/register" className="btn btn-success">register</Link> </p>
-                </div>
-            </div>
+        <div className="bg-img">
+
+          
         </div>
     </div>
   );
