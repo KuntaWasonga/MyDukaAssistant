9b79132ac5f4c1d1857c2c34e142772f112f64fd
KuntaWasonga <wkunta@rocketmail.com>
2023-06-13 13:30:27 +0300

    Updated backend APIs

--- backend/app.py
+++ backend/app.py
@@ -79,6 +79,23 @@ def create_token():
     return response
 
 
+@api.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            data = response.get_json()
+            if type(data) is dict:
+                data["access_token"] = access_token 
+                response.data = json.dumps(data)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
+
 #This API edits various properties of a client
 @api.route("/<str:person>/edit", methods=('GET', 'PUT'))
 @jwt_required
