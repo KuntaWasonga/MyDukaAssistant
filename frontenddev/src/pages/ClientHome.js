43e10b917229d484a7bf3751d64b6b9187782f7d
KuntaWasonga <wkunta@rocketmail.com>
2023-06-19 20:59:09 +0300

    Updated client home page

--- frontenddev/src/pages/ClientHome.js
+++ frontenddev/src/pages/ClientHome.js
@@ -1,37 +1,71 @@
-//This contains code for the client page of the web application
-import React, { } from "react";
+//This contains code for a user page of the web application
+import React, { useState } from "react";
+import { useLocation, useNavigate } from 'react-router-dom';
+import axios from "axios";
 
-import { Link } from 'react-router-dom';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/esm/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 
-import "./stylesheets/home.css";
+import "./stylesheets/employee.css";
 
-export default function ClientHome() {
+export default function EmployeeHome() {
 
-  //Check for the name of client htat has logged in.
-  let client = "Kunta";
+  const location = useLocation();
+  const alertMessage = location.state?.alertMessage;
 
-  //The "LOGIN" anchor is just a place holder 
-  // but should not have any color on the page.
-  
+  const navigate = useNavigate();
+
+  const [error, setError] = useState('');
+
+  const logout = () => {
+    axios.post('http://127.0.0.1:5000/user/logout', {
+    })
+      .then((response) => {
+        console.log(response);
+        const msg = response["message"]
+        navigate("/", { state: { msg } });
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  }
+
+
   return (
-    <div>
-      <div className="bg-img">
-        <div className="container">
-          <div className="top-nav">
-            <a href="#home">HOME</a>
-            <a href="#about">ABOUT</a>
-            <a href="#contact">CONTACT</a>
-            <a href="#contact">LOGIN</a>
-          </div>
-          <div className="content">
-            <h1>Welcome {client} to Jabali supermarket</h1>
-            <p>Start your shopping experience below</p>
-          </div>
-          <div className="button">
-            <button><Link to = "/scan">SCAN</Link></button>
-          </div>
-        </div>
+    <div className="box">
+      <Navbar expand="lg" bg="grey" variant="light">
+        <Container>
+          <Navbar.Brand href="/">M-Duka</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto pe-3">
+              <Nav.Link href="/">Profile</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/logout" onClick={logout}>Logout</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {alertMessage && <div className="alert">{alertMessage}</div>}
+      <div className="karibu">
+        <h1 className="message">Welcome to Jabali</h1>
       </div>
+      <Row className="justify-content-md-center">
+        <Col xs lg="2"><Button className="check" href="/scan">
+          SCAN ITEM
+        </Button>
+        </Col>
+        <Col md = "auto"></Col>
+        <Col xs lg = "2">
+        <Button className="add" href="/add">
+          
+        </Button>
+        </Col>
+      </Row>
     </div>
   );
 }
