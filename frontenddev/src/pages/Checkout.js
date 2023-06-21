59e10a31168dfd84019c0034f48b250574dc555f
KuntaWasonga <wkunta@rocketmail.com>
2023-06-21 10:59:03 +0300

    Checkout page

---
+++ frontenddev/src/pages/Checkout.js
@@ -0,0 +1,13 @@
+//Checkout page
+//Displays table of items as well as totals
+
+//paid button
+
+export default function Checkout() {
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
