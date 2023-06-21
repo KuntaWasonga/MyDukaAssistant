a9f9c4e393bbd3daa3229d7e32170af518e35419
KuntaWasonga <wkunta@rocketmail.com>
2023-06-21 10:47:37 +0300

    Update model classes

--- backend/mda/models.py
+++ backend/mda/models.py
@@ -8,8 +8,17 @@ def get_uuid():
     return uuid4().hex
 
 @login_manager.user_loader
-def load_user(client_id):
-    return User.query.get(client_id)
+def load_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        return user
+
+    employee = Employee.query.get(user_id)
+    if employee:
+        return employee
+
+    return None
+
 
 #This class defines the client of the supermarket
 class User(db.Model, UserMixin):
@@ -24,6 +33,7 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"User('{self.firstname}', '{self.email}')"
 
+
 #This class defines the employee of the supermarket
 class Employee(db.Model, UserMixin):
     __tablename__ = "employees"
@@ -33,6 +43,7 @@ class Employee(db.Model, UserMixin):
     password = db.Column(db.Text, nullable=False)
     admin = db.Column(db.Boolean, default=False)
 
+
 #This class defines a client and their items to be put on a cart
 class Cart(db.Model):
     __tablename__ = "cart"
@@ -40,8 +51,9 @@ class Cart(db.Model):
     client_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable = False)
     product = db.relationship("Product", secondary="cart_product", backref="carts", uselist=False, lazy=True) #OneToOne relationship
     quantity = db.Column(db.Integer)
-    Total = db.Column(db.Integer)
+    Total = db.Column(db.Integer, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 #This holds the products added to the database
 class Product(db.Model):
@@ -49,16 +61,18 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
     barcode = db.Column(db.Integer, primary_key=True, unique=True, nullable = False)
     name = db.Column(db.String(50), unique = True, nullable=False)
-    price = db.Column(db.Float, nullable = False)
+    price = db.Column(db.Integer, nullable = False)
     cart_id = db.Column(db.Integer, db.ForeignKey('cart.id'))
 
     def __repr__(self):
         return f"Product('{self.name}', '{self.price}')"
+
 
 # Intermediate table for the many-to-many relationship between Cart and Product
 cart_product = db.Table(
     "cart_product",
     db.Column("cart_id", db.Integer, db.ForeignKey("cart.id"), primary_key=True),
     db.Column("product_id", db.Integer, db.ForeignKey("product.id"), primary_key=True),
-    db.Column("quantity", db.Integer, nullable=False)
+    db.Column("quantity", db.Integer, nullable=False),
+    db.Column("total", db.Integer)
 )
