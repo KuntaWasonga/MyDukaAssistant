9bf5ebfdb3d44ce8c8dcfffafef5a9ba4f276f4f
KuntaWasonga <wkunta@rocketmail.com>
2023-07-04 17:29:28 +0300

    Employee routes

--- backend/mda/routes/employee_routes.py
+++ backend/mda/routes/employee_routes.py
@@ -1,3 +1,4 @@
+from flask_login import current_user
 import uuid
 from flask import Blueprint, request, jsonify
 from mda import db
@@ -12,7 +13,8 @@ employee_bp = Blueprint('employee', __name__)
 
 def get_uuid_from_integer(integer_value):
     # Convert the integer to a bytes representation
-    bytes_value = integer_value.to_bytes((integer_value.bit_length() + 7) // 8, 'big')
+    bytes_value = integer_value.to_bytes(
+        (integer_value.bit_length() + 7) // 8, 'big')
 
     # Generate a UUID from the bytes value
     uuid_value = uuid.UUID(bytes=bytes_value)
@@ -20,38 +22,38 @@ def get_uuid_from_integer(integer_value):
     return uuid_value
 
 
-#This API registers an Employee to the database
+# This API registers an Employee to the database
 @employee_bp.route("/employee/register", methods=["POST"])
 def signup():
     data = request.get_json()
     if not data:
         return jsonify({"error": "Invalid JSON"}), 400
 
-    #Check for missing fields
-    required_fields = ['id','employee_id', 'email', 'password']
+    # Check for missing fields
+    required_fields = ['id', 'employee_id', 'email', 'password']
     missing_fields = [field for field in required_fields if field not in data]
     if missing_fields:
         return jsonify({"error": f"Missing fields: {', '.join(missing_fields)}"}), 400
-    
+
     empty_fields = [field for field in required_fields if not data.get(field)]
     if empty_fields:
         return jsonify({"error": f"Empty fields: {', '.join(empty_fields)}"}), 400
 
-    #Check if the employee already exists
+    # Check if the employee already exists
     user = Employee.query.filter_by(employee_id=data['employee_id']).first()
     if user:
         return jsonify({"error": "Employee already exists"}), 409
 
-    #Ensures employee credentials are well protected
+    # Ensures employee credentials are well protected
     hashed_password = generate_password_hash(data['password'])
-    #id_uuid = get_uuid_from_integer(data['employee_id'])
+    # id_uuid = get_uuid_from_integer(data['employee_id'])
 
     new_employee = Employee(
-        id = data['id'],
-        employee_id = data['employee_id'],
-        email = data['email'],
-        password = hashed_password,
-        admin = data['admin'])
+        id=data['id'],
+        employee_id=data['employee_id'],
+        email=data['email'],
+        password=hashed_password,
+        admin=data['admin'])
 
     db.session.add(new_employee)
     db.session.commit()
@@ -59,7 +61,7 @@ def signup():
     return jsonify({"messsage": "Registration successful"})
 
 
-#This API is responsible for Employee login
+# This API is responsible for Employee login
 @employee_bp.route("/employee/login", methods=['POST'])
 def login():
     data = request.get_json()
@@ -80,15 +82,30 @@ def login():
 
     return jsonify({'message': 'Invalid credentials'}), 401
 
-#This API logs out a User
+
+# Checks if current user is logged in
+
+
+@employee_bp.route("/employee/current", methods=['GET'])
+def get_current_employee():
+    if current_user.is_authenticated:
+        return jsonify({
+            "employee_id": current_user.employee_id,
+            "email": current_user.email
+        }), 200
+
+    return jsonify({'message': 'User not logged in'}), 401
+
+
+# This API logs out a User
 @employee_bp.route('/employee/logout', methods=['POST'])
 def logout():
     logout_user()
-    return jsonify({'message' : 'Logout successful'}), 200
+    return jsonify({'message': 'Logout successful'}), 200
 
 
-#---------------------------------ADMIN ROUTES------#
-#This API lets the admin check all the users in the users database
+# ---------------------------------ADMIN ROUTES------#
+# This API lets the admin check all the users in the users database
 @employee_bp.route('/employee/users')
 @login_required
 def users():
@@ -102,26 +119,23 @@ def users():
             item_data['firstname'] = User.firstname
             item_data['lastname'] = User.lastname
             item_data['email'] = User.email
-            #item_data['password'] = User.password
+            # item_data['password'] = User.password
 
             output.append(item_data)
-            
-        return jsonify({"users" : output})
+
+        return jsonify({"users": output})
     return jsonify({"message": "unauthorized access"})
 
 
-#This API lets only an admin delete an employee from the database
-@employee_bp.route("/employee/delete/<id>", methods =['DELETE'])
+@employee_bp.route("/employee/delete/<id>", methods=['DELETE'])
 @login_required
 def delete(id):
     mtu = Employee.query.get(current_user.id)
     if mtu.admin:
-        e_id = get_uuid_from_integer(id)
-        user = Employee.query.filter_by(employee_id=e_id).first()
-        if user:
-            db.session.delete(user)
+        employee = Employee.query.filter_by(employee_id=id).first()
+        if employee:
+            db.session.delete(employee)
             db.session.commit()
-
-            return jsonify({'message': f'{user.employee_id} terminated'}), 200
-        return jsonify({'message': "Employee not found"})
-    return jsonify({'message': 'Not authorized'}), 409
+            return jsonify({'message': f'{employee.employee_id} terminated'}), 200
+        return jsonify({'message': 'Employee not found'}), 404
+    return jsonify({'message': 'Not authorized'}), 401
